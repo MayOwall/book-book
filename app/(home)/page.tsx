873a5d0cf15e5271a1bea15b4248b4f0e0969522
MayOwall <pixartive@gmail.com>
2024-02-20 19:31:57 +0900

    refactor: 새 책 생성 페이지 url 변경

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-1">
       <ReadingBookShelf />
-      <Link href="./create-new-book">
+      <Link href="./books/create">
         <LargeButton>새 책 등록하기</LargeButton>
       </Link>
     </main>
