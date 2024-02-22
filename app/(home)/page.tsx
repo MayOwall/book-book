9682c60dba05874bda8e37945a37f407c0e6852f
MayOwall <pixartive@gmail.com>
2024-02-22 14:38:16 +0900

    refactor: ReadingBookshelf 내부의 localStorage 로직을 외부 스코프로 변경

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,33 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ReadingBookShelf, LargeButton } from "@/src/components";
-import { bookinfo } from "@/src/types";
 
 export default function Home() {
-  const [selectedbook, setSelectedbook] = useState<bookinfo | null>(null);
-  const handleSelectedBook = (bookinfo: bookinfo) => {
-    setSelectedbook(() => bookinfo);
+  const [readingbooks, setReadingbooks] = useState([]);
+  const [selectedbookIsbn, setSelectedbookIsbn] = useState<string | null>(null);
+
+  const handleSelectedBook = (isbn: string) => {
+    setSelectedbookIsbn(() => (isbn === selectedbookIsbn ? null : isbn));
   };
+
+  useEffect(() => {
+    const localReadingbooks = localStorage.getItem("readingbooks");
+    const readingbooks = localReadingbooks ? JSON.parse(localReadingbooks) : [];
+    setReadingbooks(() => readingbooks);
+  }, []);
+
   return (
     <main className="flex flex-col gap-1">
-      <ReadingBookShelf handleSelectedBook={handleSelectedBook} />
+      <ReadingBookShelf
+        readingbooks={readingbooks}
+        onClick={handleSelectedBook}
+      />
       <Link href="./books/create">
         <LargeButton>새 책 등록하기</LargeButton>
       </Link>
-      {JSON.stringify(selectedbook)}
+      {selectedbookIsbn}
     </main>
   );
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,8 @@
 import { ReactNode } from "react";
 
 interface ReadingBookShelf {
-  handleSelectedBook: (bookinfo: bookinfo) => void;
+  readingbooks: bookinfo[];
+  onClick: (isbn: string) => void;
 }
 
 interface LargeButton {
