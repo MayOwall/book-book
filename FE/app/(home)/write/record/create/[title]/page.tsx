253e448e66f19afac9dd7172aa0eb55abf4ff554
MayOwall <pixartive@gmail.com>
2024-03-14 18:47:59 +0900

    feat : 독서 페이지 검증 로직 구현

--- FE/app/(home)/write/record/create/[title]/page.tsx
+++ FE/app/(home)/write/record/create/[title]/page.tsx
@@ -36,8 +36,17 @@ export default function CreateRecord() {
   // 제출시의 로직
   const onSubmit = () => {
     if (!startPage || !endPage) {
-      alert("페이지를 입력해주세요");
-      return;
+      return alert("페이지를 입력해주세요.");
+    }
+
+    if (isNotNumber(startPage, endPage)) {
+      return alert("숫자만 입력해주세요.");
+    }
+
+    if (isStartBiggerThanEnd(startPage, endPage)) {
+      return alert(
+        "마지막 페이지는 시작 페이지 보다 크거나 같아야 합니다. 다시 입력해주세요.",
+      );
     }
 
     router.push(
@@ -47,23 +56,23 @@ export default function CreateRecord() {
 
   return (
     <main className="relative  flex h-full flex-col gap-4 py-16">
-      <h1 className="mb-4 text-2xl font-bold">페이지 정보를 입력해주세요</h1>
+      <h1 className="mb-4 text-2xl font-bold">읽은 페이지를 입력해주세요</h1>
       <input
-        className="w-3/4 outline-none"
+        className="h-10 w-full rounded px-2 outline-emerald-400"
         type="number"
         value={startPage}
         onChange={handleStartPage}
         placeholder="시작 페이지를 입력해주세요"
       />
       <input
-        className="w-3/4 outline-none"
+        className="h-10 w-full rounded px-2 outline-emerald-400"
         type="number"
         value={endPage}
         onChange={handleEndPage}
         placeholder="마지막 페이지를 입력해주세요"
       />
       <div className="absolute bottom-28 flex w-full flex-col justify-center gap-4">
-        <Button onClick={onSubmit}>페이지 입력하기</Button>
+        <Button onClick={onSubmit}>페이지 입력 완료</Button>
         <Link href="/write" className="w-full text-center">
           <button className=" text-neutral-400">입력 취소</button>
         </Link>
@@ -71,3 +80,11 @@ export default function CreateRecord() {
     </main>
   );
 }
+
+const isNotNumber = (start: number, end: number) => {
+  return isNaN(start) || isNaN(end);
+};
+
+const isStartBiggerThanEnd = (start: number, end: number) => {
+  return start > end;
+};
