4ccba9cc25ed15bf9f82f3d51bf74f61caca9c65
MayOwall <pixartive@gmail.com>
2024-04-06 21:36:53 +0900

    feat: FNB 컴포넌트 구현 - book-fill 외 FNB 관련 아이콘 5개 추가 - FNB 컴포넌트 구현

---
+++ FE/src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+import BookFill from "@/src/assets/icons/book-fill-icon.svg";
+import BookLine from "@/src/assets/icons/book-line-icon.svg";
+import CalendarFill from "@/src/assets/icons/calendar-fill-icon.svg";
+import CalendarLine from "@/src/assets/icons/calendar-line-icon.svg";
+import GearFill from "@/src/assets/icons/gear-fill-icon.svg";
+import GearLine from "@/src/assets/icons/gear-line-icon.svg";
+
+export { BookFill, BookLine, CalendarFill, CalendarLine, GearFill, GearLine };
---
+++ FE/src/components/molecules/FNB.tsx
@@ -0,0 +1,43 @@
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import * as Icon from "@/src/assets/icons";
+
+export default function FNB() {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex h-12 w-full items-center justify-evenly bg-white shadow-lg">
+      <Item type="book" selected={pathname === "/book"} />
+      <Item type="log" selected={pathname === "/log"} />
+      <Item type="setting" selected={pathname === "/setting"} />
+    </div>
+  );
+}
+
+function Item({
+  type,
+  selected,
+}: {
+  type: "book" | "log" | "setting";
+  selected: boolean;
+}) {
+  return (
+    <Link href={`/${type}`}>
+      <div className="flex w-16 flex-col items-center">
+        {type === "book" && selected && <Icon.BookFill />}
+        {type === "book" && !selected && <Icon.BookLine />}
+        {type === "log" && selected && <Icon.CalendarFill />}
+        {type === "log" && !selected && <Icon.CalendarLine />}
+        {type === "setting" && selected && <Icon.GearFill />}
+        {type === "setting" && !selected && <Icon.GearLine />}
+        <div
+          className={`text-small-regular text-${selected ? "[theme(colors.primary.default)]" : "gray-300"}`}
+        >
+          {type === "book" && "도서 목록"}
+          {type === "log" && "독서 기록"}
+          {type === "setting" && "설정"}
+        </div>
+      </div>
+    </Link>
+  );
+}
